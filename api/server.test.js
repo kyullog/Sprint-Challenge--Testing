26da96aedb4dc724f4896a8844dc07833afca936
Jonathan Dorety <kyullog@gmail.com>
2019-04-20 12:45:07 -0400

    Establish test framework for POST /games endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -9,4 +9,11 @@ describe("The server API", () => {
       expect(res.status).toBe(200);
     });
   });
+
+  describe("POST /games", () => {
+    describe("it should take in an object with title, genre, and releaseYear key/value pairs", () => {});
+    it("should add an object to the data array if correctly shaped object is passed", async () => {});
+    it("should return status code 201 if correctly shaped object is passed", async () => {});
+    it("should return a 422 status code if an incorrectly shaped object is passed", async () => {});
+  });
 });
