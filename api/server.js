e4833d7c968a9e5f085150df7a8b43e87fcd8575
Jonathan Dorety <kyullog@gmail.com>
2019-04-20 12:59:26 -0400

    Create test to see if item is added on post

--- api/server.js
+++ api/server.js
@@ -7,4 +7,13 @@ server.get("/", (req, res) => {
   res.status(200).json({ message: "Hello!" });
 });
 
+server.post("/games", (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  if (title && genre && releaseYear) {
+    res.status(201).json({ message: "Game added" });
+  } else {
+    res.status(422).json({ message: "Please use correct format" });
+  }
+});
+
 module.exports = server;
--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,6 @@
 const server = require("./server.js");
 const request = require("supertest");
+const data = require("../data/dummyData.js");
 
 describe("The server API", () => {
   describe("GET /", () => {
@@ -11,9 +12,36 @@ describe("The server API", () => {
   });
 
   describe("POST /games", () => {
-    describe("it should take in an object with title, genre, and releaseYear key/value pairs", () => {});
-    it("should add an object to the data array if correctly shaped object is passed", async () => {});
-    it("should return status code 201 if correctly shaped object is passed", async () => {});
-    it("should return a 422 status code if an incorrectly shaped object is passed", async () => {});
+    describe("should take in an object with title, genre, and releaseYear key/value pairs", () => {
+      it("should return status code 201 if correctly shaped object is passed", async () => {
+        const game = {
+          title: "Donkey Kong",
+          genre: "Arcade",
+          releaseYear: 1983
+        };
+        const res = await request(server)
+          .post("/games")
+          .send(game);
+        expect(res.status).toBe(201);
+      });
+      it("should return a 422 status code if an incorrectly shaped object is passed", async () => {
+        const game = { title: "Angry Birds" };
+        const res = await request(server)
+          .post("/games")
+          .send(game);
+        expect(res.status).toBe(422);
+      });
+      it("should add an object to the data array if correctly shaped object is passed", async () => {
+        const game = {
+          title: "Redneck Rampage",
+          genre: "FPS",
+          releaseYear: 1996
+        };
+        await request(server)
+          .post("/games")
+          .send(game);
+        expect(data[data.length - 1]).toEqual(game);
+      });
+    });
   });
 });
